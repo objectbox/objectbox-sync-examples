3c029ab0373a088a0825e79bf06f9dcd919e86df
Uwe <noreply+uwe@objectbox.io>
2025-03-17 11:37:35 +0100

    client-java: mark ObjectBox specific changes in build sripts

--- tasks/client-java/app/build.gradle.kts
+++ tasks/client-java/app/build.gradle.kts
@@ -1,21 +1,22 @@
-// See the root build script on how to add plugins and repositories.
+// For ObjectBox: see the root build script on how to add required plugins and repositories
 
 plugins {
     id("application")
 }
 
 java {
+    // For ObjectBox: the Java SDK requires at least Java 8
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks.withType<JavaCompile> {
-    // Note: Gradle defaults to the platform default encoding, make sure to always use UTF-8.
+    // Gradle defaults to the platform default encoding, make sure to always use UTF-8
     options.encoding = "UTF-8"
 }
 
 tasks.named<JavaExec>("run") {
-    // Let Gradle run accept console input
+    // Support console input
     standardInput = System.`in`
 }
 
@@ -23,16 +24,22 @@ application {
     mainClass.set("io.objectbox.example.Main")
 }
 
+// For ObjectBox: reference common version defined in root built script
 val objectboxVersion: String by rootProject.extra
 
 dependencies {
-    // Optional: include all native libraries for distribution
-    // (only the one for the current platform is added by the plugin).
+    // For ObjectBox: add the Java SDK
+    implementation("io.objectbox:objectbox-java:$objectboxVersion")
+
+    // For ObjectBox: add libraries for all platforms supported by your application.
+    // Note that the ObjectBox Gradle plugin would automatically add the one matching your current machine.
+    // Also, libraries for more platforms than added below are available, check Maven Central.
     implementation("io.objectbox:objectbox-sync-windows:$objectboxVersion")
     implementation("io.objectbox:objectbox-sync-linux:$objectboxVersion")
     implementation("io.objectbox:objectbox-sync-macos:$objectboxVersion")
 }
 
-// Apply the plugin after the dependencies block so it detects added
-// ObjectBox dependencies and does not replace them.
+// For ObjectBox: apply the Sync plugin.
+// Note that the plugin is applied after the dependencies block so it can detect
+// manually added ObjectBox dependencies.
 apply(plugin = "io.objectbox.sync")
