0461ab20863fd9c65cc3d59caf586cbf26787fad
Shubham Panchal <panchalshubhamdev@gmail.com>
2025-03-21 16:43:29 +0530

    server-docker: add sync server version param in start.sh and start.bat

--- tasks/server-docker/start-cli.bat
+++ tasks/server-docker/start-cli.bat
@@ -1,3 +1,19 @@
+@echo off
+
+rem Check if version parameter is provided
+if "%1"=="" goto noversion
+set sync_server_version=%1
+goto continue
+
+:noversion
+echo Version not given (first parameter to this script). Using default version.
+set sync_server_version=2025-03-10
+
+:continue
+echo Starting ObjectBox Sync Server version %sync_server_version%
+echo Stop the server with Ctrl-C
+echo --------------------------------------------------------------------------
+
 docker run --rm -it ^
     --volume "%cd%:/data" ^
     --publish 127.0.0.1:9999:9999 ^
