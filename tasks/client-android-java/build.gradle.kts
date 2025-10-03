a264dca48fe69c42b1f7a8a7d6b5f4b43973043d
Markus <markus@greenrobot>
2025-10-03 12:40:49 +0200

    client-android-*: update to ObjectBox 5.0.1

--- tasks/client-android-java/build.gradle.kts
+++ tasks/client-android-java/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 buildscript {
     // For ObjectBox: define common version for tools and dependencies
-    val objectboxVersion by extra("4.3.0")
+    val objectboxVersion by extra("5.0.1")
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
