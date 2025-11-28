3e974112de4c409f7edde1b03799f66f6e314489
Markus <markus@greenrobot>
2025-11-28 11:29:31 +0100

    tasks/server-docker: fix version in start-cli.bat

--- tasks/server-docker/start-cli.bat
+++ tasks/server-docker/start-cli.bat
@@ -18,7 +18,7 @@ docker run --rm -it ^
     --volume "%cd%:/data" ^
     --publish 127.0.0.1:9999:9999 ^
     --publish 127.0.0.1:9980:9980 ^
-    objectboxio/sync-server-trial ^
+    objectboxio/sync-server-trial:%sync_server_version% ^
     --model /data/objectbox-model.json ^
     --unsecured-no-authentication ^
     --admin-bind 0.0.0.0:9980
